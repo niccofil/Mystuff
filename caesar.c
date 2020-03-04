1a915bd125bca999da7f4165dc4a28f1de7fbecb
niccofil <58049232+niccofil@users.noreply.github.com>
2020-03-04 20:37:03 +0200

    pset2

---
+++ caesar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <stdlib.h>
+
+int main(int argc, string argv[])
+{
+    if (argc == 2)
+    {
+        printf("Succes\n");
+    }
+    else
+    {
+        printf("Usage: ./caesar key\n");
+    }
+}
