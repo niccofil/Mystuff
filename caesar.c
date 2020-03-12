a23e8ab9f29e5f860744ab703cd729bf9cd030a6
niccofil <58049232+niccofil@users.noreply.github.com>
2020-03-12 17:48:07 +0200

    c

--- caesar.c
+++ caesar.c
@@ -4,7 +4,7 @@
 #include <ctype.h>
 #include <string.h>
 
-int main(int argc, string argv[])
+int main(int argc, string argv[]) // input only 2 cmd line arguments
 {
     if (argc == 2)
     {
@@ -16,28 +16,52 @@ int main(int argc, string argv[])
             c = isdigit(argv[1][i]);
         }
 
-        if (c > 0 && k > 0)
+        if (c > 0 && k > 0) //argv is digit
         {
             string text = get_string("plaintext:");
-            for (int j = 0; j < strlen(text); j++)
+            printf("ciphertext:");
+
+            for (int j = 0; j < strlen(text); j++) // cipher transformation
             {
+                int alpha;
+                int ci;
+                int out;
+
                 if (isalpha(text[j]) != 0 && islower(text[j]) != 0)
                 {
-                    int alpha = text[j] % 97;
-                    int ci = (alpha + k) % 26;
-                    printf("ciphertext: %i\n", ci);
+                    alpha = text[j] % 97;  //alphabetical number
+                    ci = (alpha + k) % 26; // ciphered alph value
+                    out = ci + 97;         // ASCII value
+                    printf("%c", out);
+                }
+
+                if (isalpha(text[j]) != 0 && isupper(text[j]) != 0)
+                {
+                    alpha = text[j] % 65;
+                    ci = (alpha + k) % 26;
+                    out = ci + 65;
+                    printf("%c", out);
+                }
+
+                if (ispunct(text[j]) != 0 || isspace(text[j]) != 0)
+                {
+                    out = text[j];
+                    printf("%c", out);
                 }
             }
+            printf("\n");
         }
 
         else
         {
             printf("usage: ./caesar key\n");
+            return 1;
         }
     }
 
     else
     {
         printf("usage: ./caesar key\n");
+        return 1;
     }
 }
