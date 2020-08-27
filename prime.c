f9828ebf3d6ff7d966b4721579dcac852e1f8e41
niccofil <nikita.ttu@gmail.com>
2020-08-27 21:46:56 +0300

    primes up to 100

---
+++ prime.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(void)
+{
+   int x, y;
+
+   printf("insert first number:");
+   scanf("%i", &x);
+
+   printf("insert second number:");
+   scanf("%i", &y);
+
+   while (x <= y)
+   {
+      if ((x == 2 || x == 5 || x == 7 || x == 3) || (x != 1 && x % 2 != 0 && x % 3 != 0 && x % 7 != 0 && x % 10 != 5))
+      {
+         printf("%d\t", x);
+      }
+
+      x++;
+   }
+
+   printf("\n");
+}
