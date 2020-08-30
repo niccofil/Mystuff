8501569e7ff7ec91211c2e2e8f810ff3b2eaf30e
niccofil <nikita.ttu@gmail.com>
2020-08-30 21:40:08 +0300

    hello tabed

--- hello.c
+++ hello.c
@@ -5,14 +5,14 @@ int main (int argc, char* argv[])
 {
 if (argc != 2)
 {
-printf ("Please, insert the name of AI\n");
-return 1;
+	printf ("Please, insert the name of AI\n");
+	return 1;
 }
 
 if (strcmp(argv[1], "astra") != 0)
 {
-printf ("It is not %s. It is actually astra\n", argv[1]);
-return 1;
+	printf ("It is not %s. It is actually astra\n", argv[1]);
+	return 1;
 }
 
 char name[20];
@@ -27,12 +27,12 @@ scanf ("%i", &age);
 
 if (age > 60)
 {
-printf ("Almost like my grandpa\n");
+	printf ("Almost like my grandpa\n");
 }
 
 else
 {
-printf("So young! Only %i years old!\n", age);
+	printf("So young! Only %i years old!\n", age);
 }
-return 0;
+	return 0;
 }
