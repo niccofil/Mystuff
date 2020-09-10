e3de0d3200eea41c822574dbe0f08d305aa0dde9
niccofil <nikita.ttu@gmail.com>
2020-09-10 17:09:54 +0300

    temperature converter

---
+++ temperature.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char* argv[])
+{
+
+    if(argc != 2)
+    {
+        printf("usage: temperature c/f\n");
+        return 1;
+    }
+
+    if(strcmp(argv[1], "c") != 0 && strcmp(argv[1], "f") != 0)
+    {
+        printf("usage: temperature c/f\n");
+        return 2;
+    }
+
+    float f, c;
+
+    if (strcmp(argv[1], "c") == 0)
+    {
+        printf("Enter the temperature in fahrenheit:");
+        scanf("%f", &f);
+
+        c = (f - 32) * 5 / 9;
+
+        printf("Your temperature in celcius is:%.1f\n", c);
+
+        return 0;
+    }
+
+    if (strcmp(argv[1], "f") == 0)
+    {
+        printf("Enter the temperature in celsius:");
+        scanf("%f", &c);
+
+        f = (c * 9/5) + 32;
+
+        printf("Your temperature in fahrenheit is:%.1f\n", f);
+
+        return 0;
+    }
+}
