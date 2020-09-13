a388b201394a31b60ff7ba456ef519c48a2ca2c1
niccofil <nikita.ttu@gmail.com>
2020-09-13 09:15:56 +0300

    bubble sort

--- bubble.c
+++ bubble.c
@@ -1,45 +1,27 @@
 #include <stdio.h>
-void swap(int *a, int *b);
-void bubble_sort(int a[], int n);
-void print_array(int a[], int n);
 
 int main(void)
 {
-    int array[] = {1, 10, 7, 5, 6, 3, 4, 2};
+    int array[] = {9, 6, 4};
     int n = sizeof(array) / sizeof(int);
-
-    bubble_sort(array, n);
-    print_array(array, n);
-}
-
-void swap(int *a, int *b)
-{
-    int temp = *a;
-    *a = *b;
-    *b = temp;
-}
-
-void bubble_sort(int a[], int n)
-{
 
     for (int i = 0; i < n - 1; i++)
     {
         for (int j = 0; j < n - i - 1; j++)
         {
-            if (a[j] > a[j + 1])
+
+            if (array[j] > array[j + 1])
             {
-                swap(&a[j], &a[j + 1]);
+                int temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
             }
         }
     }
-}
-
-void print_array(int a[], int n)
-{
 
     for (int i = 0; i < n; i++)
     {
-        printf("%i\t", a[i]);
+        printf("%i\t", array[i]);
     }
     printf("\n");
 }
