5d56d5981a66a00351e1befb0894aa4702c6cef8
niccofil <nikita.ttu@gmail.com>
2020-09-15 20:10:16 +0300

    struct.c

--- struct.c
+++ struct.c
@@ -3,24 +3,41 @@
 int main(void)
 {
   typedef struct{
-	  int score;
-	  char*name;
-  }student;
+	  float score;
+	  char name[20];
+  }students;
 
   int n;
+  float sum = 0;
 
   printf("number of students:");
   scanf("%i",&n);
-  student student[n].score;
+  students student[n];
 
   for (int i = 0; i < n; i++)
-  {
+  {     printf("Student %i name:", i+1);
+	scanf("%s",student[i].name);
+
 	printf("Student %i score:",i+1);
-	scanf("%i",student[i].score);
+	scanf("%f",&student[i].score);
+        sum += student[i].score;
   }
-  
-  int mean = student[n].score / n;
-  printf("%i\n", mean);
+  for (int i = 0; i < n - 1; i++)
+  {
+	for (int j = 0; j < n - 1 - i; j++)
+	{
+		if (student[j].score > student[j + 1].score)
+		{
+			int tmp = student[j].score;
+			student[j].score = student[j + 1].score;
+			student[j+1].score = tmp;
+		}
+	}
+  }	
+  float mean = sum / n;
+
+  printf("The best score is %i by %s", student[i].score, student[i].name);
+  printf("Average score is %.1f\n", mean);
 }
 
 
